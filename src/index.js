8cefe207d827961aed31bf970e7efd789b2323e7
ground-cinnamon <morkovog@gmail.com>
2024-09-30 18:24:33 +0300

    feat: make a task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+// module.exports = function towelSort(matrix) {
+//     if (matrix === undefined) {
+//         return [];
+//     }
+//     let result = [];
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
+//             if (i === 0 || i % 2 === 0) {
+//                 result.push(matrix[i][j]);
+//             } else {
+//                 result.push(matrix[i][matrix[i].length - 1 - j]);
+//             }
+//         }
+//     }
+
+//     return result;
+// };
+
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+
+    return matrix.flatMap((row, index) => {
+        if (index % 2 !== 0) return row.reverse();
+        return row;
+    });
+};
